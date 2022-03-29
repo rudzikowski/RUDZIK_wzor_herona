5a4f8f6781563ab164615ce3bccb023c4d704097
Artur <artur.rudzik@o2.pl>
2022-03-29 16:42:48 +0200

    Add project files.

---
+++ RUDZIK_wzor_herona/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RUDZIK_wzor_herona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Witaj w aplikacji obliczającej pole trójkąta według wzoru herona");
+            Console.WriteLine("Podaj bok 1");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj bok 2");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj bok 3");
+            c = double.Parse(Console.ReadLine());
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                double p =(a + b + c)/2;
+                double dopierwiastka = p * (p - a) * (p - b) * (p - c);
+                double Pole = Math.Sqrt(dopierwiastka);
+                Console.WriteLine("Pole tego trójkąta wynosi {0}",Pole);
+            }
+            else
+            {
+                Console.WriteLine("Trójkąt nie może powstać");
+            }
+            Console.ReadKey();
+        }
+    }
+}
